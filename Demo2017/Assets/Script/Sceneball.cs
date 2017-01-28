bd31af734432152d952bc8e581921c396ff16acf
Suttikiat Songseang <suttikiat.song@bumail.net>
2017-01-28 16:59:12 +0700

    no message

---
+++ Demo2017/Assets/Script/Sceneball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Sceneball : MonoBehaviour {
+	public Text txtScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	int score = 0;
+	public void Push1(){
+		score++;
+		txtScore.text = "Score :" + score.ToString ();
+	}
+
+
+	public void Dochange (){
+		SceneManager.LoadScene (0);
+	}
+}
